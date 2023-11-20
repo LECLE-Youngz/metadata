abbc0691f00454c6e58a8b6a4afb21d68f83f911
Kiet1618 <ngokiet3207@gmail.com>
2023-11-20 11:29:14 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors(
+    {
+      origin: "*", // Allow all domains
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    }
+  );
   app.useGlobalPipes(new ValidationPipe());
 
   const port = 8080;
