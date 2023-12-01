aced25436060d5a84bedfdf70af52c37d4c8875b
Kiet1618 <ngokiet3207@gmail.com>
2023-12-02 02:09:00 +0700

    fix owner data

--- src/controllers/data.controller.ts
+++ src/controllers/data.controller.ts
@@ -16,7 +16,7 @@ import { Data, User } from "src/schemas";
 //   import { getCurrentPromptBuyer, getCurrentPromptPrice } from "src/utils/blockchain";
 import { verifyAccessToken } from "src/auth/google.verifier";
 import BN from "bn.js"
-import { fetchWalletByAddress, getPromptPrice, getTokenPrice, ownerOf, queryListAllower, queryNFTsByAddress, queryPromptAllowerByTokenAndAddress } from "src/api";
+import { fetchWalletByAddress, getPromptPrice, getTokenPrice, ownerOf, queryListAllower, queryNFTsByAddress, queryPromptAllowerByTokenAndAddress, queryPromptBuyerByTokenAndAddress } from "src/api";
 import { NftCollection } from "src/types";
 
 @Controller("api/v1/data")
@@ -62,7 +62,36 @@ export class DataController {
 
         const listData = await queryPromptAllowerByTokenAndAddress(wallet.data.address);
         const listDataInfo = await this.dataService.findDataByListIdAndCollection(listData.map(data => ({ id: data.tokenId, addressCollection: data.contract })));
-        return listDataInfo;
+        const listNft = await this.nftService.findNftsByListObjectIdWithCollection(listData.map(data => ({ id: data.tokenId, addressCollection: data.contract })));
+
+        const listDataWithNft = await Promise.all(listDataInfo.map(async data => {
+            const nft = listNft.find(nft => nft.id === data.id && nft.addressCollection === data.addressCollection);
+            const price = await getTokenPrice(nft.addressCollection, String(nft.id));
+            const promptPrice = await getPromptPrice(nft.addressCollection, String(nft.id));
+            const listAllower = await queryListAllower(nft.addressCollection, nft.id);
+            const listBoughts = await queryPromptBuyerByTokenAndAddress(nft.addressCollection, nft.id);
+            return {
+                id: nft.id,
+                addressCollection: nft.addressCollection,
+                name: nft.name,
+                description: nft.description,
+                meta: data.meta,
+                image: nft.image,
+                price: {
+                    avax: price[0].toString(),
+                    usd: price[1].toString()
+                },
+                promptPrice: {
+                    avax: promptPrice[0].toString(),
+                    usd: promptPrice[1].toString()
+                },
+                promptBuyer: listBoughts,
+                promptAllower: listAllower,
+                attributes: nft.attributes,
+                owner: user
+            }
+        }))
+        return listDataWithNft;
     }
 
 
