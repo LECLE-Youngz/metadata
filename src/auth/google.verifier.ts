3727e491043ed8c0adc3c7ba6b203f608719426f
Kiet1618 <ngokiet3207@gmail.com>
2023-12-04 01:35:04 +0700

    remove axios

--- src/auth/google.verifier.ts
+++ src/auth/google.verifier.ts
@@ -1,15 +1,22 @@
-import axios from "axios";
-import { User } from "../schemas";
-export async function verifyAccessToken(accessToken: string) {
-    const userinfoUrl = "https://www.googleapis.com/oauth2/v1/userinfo";
-    const response = await axios.get(userinfoUrl, {
-        headers: {
-            Authorization: accessToken,
-        },
-    });
-    if (response.status !== 200) {
-        throw new Error("Failed to verify access token");
-    }
-    return await response.data as User;
+import { request } from 'gaxios';
+import { User } from '../schemas';
 
+export async function verifyAccessToken(accessToken: string) {
+    const userinfoUrl = 'https://www.googleapis.com/oauth2/v1/userinfo';
+    const headers = {
+        Authorization: accessToken,
+    };
+
+    try {
+        const response = await request<User>({
+            url: userinfoUrl,
+            headers,
+        });
+
+        // Assuming the response.data is of type User
+        return response.data;
+    } catch (error) {
+        // Handle errors
+        throw new Error('Failed to verify access token');
+    }
 }
