9c0fc3c6d1c3fb2dbaf2ae241bf86740f4865619
Kiet1618 <ngokiet3207@gmail.com>
2023-12-06 21:22:52 +0700

    fix

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,6 @@
 export * from "./user.service"
 export * from "./nft.service"
 export * from "./data.service"
-export * from "./wallet.service"
 export * from "./post.service"
 export * from "./comment.service"
 export * from "./social-user.service"
