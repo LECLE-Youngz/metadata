f3262465a8ed436cb36edcc5a8896b6fe6c20f84
Kiet1618 <ngokiet3207@gmail.com>
2023-12-08 01:44:32 +0700

    fi

--- src/controllers/enft.controller.ts
+++ src/controllers/enft.controller.ts
@@ -19,15 +19,56 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import BN from "bn.js"
-import Web3 from "web3";
 
-@Controller("api/v1/nfts")
-export class NftController {
+@Controller("api/v1/enfts")
+export class ENftController {
 
     constructor(
         private readonly userService: UserService,
-        private readonly eNftService: ENftService,
+        private readonly eNftService: ENftService
     ) { }
+
+    @Get()
+    async getAllENfts(@Headers("authorization") authorization: string) {
+        const nfts = await this.eNftService.findAll();
+        const nftsWithOwners = await Promise.all(
+            nfts.map(async (nft) => {
+                const user = await verifyAccessToken(authorization);
+                const wallet = await fetchWalletByAddress(user.email);
+                if (!wallet.data.owner) {
+                    throw new BadRequestException(`Wallet does not exist`);
+                }
+                const deployer = await queryAllCollectionByAddressAPI(nft.addressCollection);
+                const subscriber = await querySubscriberAPI(deployer);
+                if (subscriber.includes(wallet.data.owner.toLocaleLowerCase())) {
+                    const userInfo = await this.userService.findUserByEmail(wallet.data.owner);
+                    const price: Array<BN> = await getTokenPrice(nft.addressCollection, String(nft.id))
+                    return {
+                        id: nft.id,
+                        name: nft.name,
+                        description: nft.description,
+                        image: nft.image,
+                        price: {
+                            avax: price[0].toString(),
+                            usd: price[1].toString(),
+                        },
+                        owner: {
+                            id: userInfo.id,
+                            name: userInfo.name,
+                            email: userInfo.email,
+                            picture: userInfo.picture,
+                            address: deployer,
+                        },
+                        addressCollection: nft.addressCollection,
+                        attributes: nft.attributes,
+                        meta: nft.meta,
+                    };
+                }
+            })
+        );
+
+        return nftsWithOwners
+    }
 
 
     @Get("/collection")
@@ -36,19 +77,26 @@ export class NftController {
         return listCollection;
     }
 
-    @Get("/collection/:addressCollection")
-    async getNftByAddressCollection(@Param("addressCollection") addressCollectionRaw: string) {
-        const addressCollection = addressCollectionRaw.toLowerCase();
+
+    @Get("/collection/:id")
+    async getNftByAddressCollection(@Param("id") id: string, @Headers("authorization") authorization: string) {
+        const userDeployer = await this.userService.findUserById(id);
+        const walletDeployer = await fetchWalletByAddress(userDeployer.email);
+        if (!walletDeployer.data.owner) {
+            throw new BadRequestException(`Wallet does not exist`);
+        }
+        const addressCollection = walletDeployer.data.address;
         const listNftId = await this.eNftService.getENftByAddressCollection(addressCollection);
         const listNft = await Promise.all(listNftId.map(async (id) => {
             const nft = await this.eNftService.findENftByIdAndAddressCollection(id, addressCollection);
-            const addressOwner: string = await ownerOf(nft.id, nft.addressCollection);
-            const wallet = await fetchWalletByAddress(addressOwner);
-            if (wallet) {
-                const userInfo = await this.userService.findUserByEmail(wallet.data.owner);
+            const user = await verifyAccessToken(authorization);
+            const walletUser = await fetchWalletByAddress(user.email);
+            if (!walletUser.data.owner) {
+                throw new BadRequestException(`Wallet does not exist`);
+            }
+            const listSubscriber = await querySubscriberAPI(addressCollection);
+            if (listSubscriber.includes(walletUser.data.owner.toLocaleLowerCase())) {
                 const price: Array<BN> = await getTokenPrice(nft.addressCollection, String(nft.id))
-                const listAllower = await queryListAllower(nft.addressCollection, nft.id);
-                const listBoughts = await queryPromptBuyerByTokenAndAddress(nft.addressCollection, nft.id);
                 return {
                     id: nft.id,
                     name: nft.name,
@@ -59,58 +107,18 @@ export class NftController {
                         usd: price[1].toString(),
                     },
                     owner: {
-                        id: userInfo.id,
-                        name: userInfo.name,
-                        email: userInfo.email,
-                        picture: userInfo.picture,
-                        address: addressOwner,
+                        id: userDeployer.id,
+                        name: userDeployer.name,
+                        email: userDeployer.email,
+                        picture: userDeployer.picture,
+                        address: addressCollection,
                     },
-                    promptBuyer: listBoughts,
                     addressCollection: nft.addressCollection,
-                    promptAllower: listAllower,
                     attributes: nft.attributes,
                     meta: nft.meta,
                 };
             }
         }))
         return listNft;
-    }
-
-
-    @Get(":id/collection/:addressCollection")
-    async getNftById(@Param("id") id: number, @Param("addressCollection") addressCollectionRaw: string) {
-        const addressCollection = addressCollectionRaw.toLowerCase();
-        const nfts = await this.eNftService.findENftByIdAndAddressCollection(id, addressCollection);
-        if (!nfts) {
-            throw new BadRequestException(`Nft does not exist`);
-        }
-        const addressOwner: string = await ownerOf(id, addressCollection);
-        const wallet = await fetchWalletByAddress(addressOwner);
-        if (!wallet) {
-            throw new BadRequestException(`Wallet does not exist`);
-        }
-        const price: Array<BN> = await getTokenPrice(nfts.addressCollection, String(nfts.id))
-        const ownerInfo = await this.userService.findUserByEmail(wallet.data.owner);
-        const listAllower = await queryListAllower(nfts.addressCollection, nfts.id);
-        const listBoughts = await queryPromptBuyerByTokenAndAddress(nfts.addressCollection, nfts.id);
-
-        return {
-            id: nfts.id,
-            name: nfts.name,
-            description: nfts.description,
-            image: nfts.image,
-            price: {
-                avax: price[0].toString(),
-                usd: price[1].toString(),
-
-            },
-            owner: ownerInfo,
-            promptBuyer: listBoughts,
-            addressCollection: nfts.addressCollection,
-            promptAllower: listAllower,
-            attributes: nfts.attributes,
-            meta: nfts.meta,
-        };
-
     }
 }
